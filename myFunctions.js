0f84251eedb29a211ae7e8cf0a4c7bf647b348fb
Axurj <acerj2468@gmail.com>
2020-07-09 14:28:19 -0700

    Pokemon function Update: The Pokemon now rotate correctly

--- myFunctions.js
+++ myFunctions.js
@@ -7,28 +7,54 @@ let pokeArray = [
   ];
 
 let index = 0;
-document.getElementById('myButton').addEventListener('click', pokeCounting1);
+document.getElementById('myButton').addEventListener('click', pokeCounting);
 
 // Box 1 
-function pokeCounting1() {
-  // Gets + Shows name of Pokemon
+function pokeCounting() {
+  // Gets name of Pokemon for Box 1
   let nameElement = document.getElementById('pokeNames');
-  nameElement.innerHTML = pokeArray[index].name;
-  // Gets + Shows Pokemon Image
+  // Gets Pokemon Image for Box 1
   let picElement = document.getElementById('pokePics');
-  picElement.src = pokeArray[index].image;
+  // Gets Index Number
   let numElement = document.getElementById('iNum');
-  numElement.innerHTML = index;
-  index++;
-}
-
-// Box 2
-function pokeCounting2() {
+  // Gets name of Pokemon in Box 2
   let nameElement2 = document.getElementById('pokeNames2');
-  nameElement2.innerHTML = pokeArray[index].name;
+  // Gets Picture of Pokemon in Box 2
   let picElement2 = document.getElementById('pokePics2');
-  picElement2.src = pokeArray[index].image;
+  // Gets Index Number in Box 2
   let numElement2 = document.getElementById('num2');
-  numElement2.innerHTML = index;
+  if(index == 0) {
+    // Shows image + name of Pokemon at Box 1
+    nameElement.innerHTML = pokeArray[index].name;
+    picElement.src = pokeArray[index].image;
+    // For testing index in Box 1
+    numElement.innerHTML = index;
+  } else {
+    // Shows image + name of Pokemon in Box 1
+    nameElement.innerHTML = pokeArray[index].name;
+    picElement.src = pokeArray[index].image;
+    // Testing index in Box 1
+    numElement.innerHTML = index;
+    // Shows image + name of Pokemon in Box 2
+    nameElement2.innerHTML = pokeArray[index-1].name;
+    picElement2.src = pokeArray[index-1].image;
+    // Testing Index in Box 2
+    numElement2.innerHTML = index;
+  }
   index++;
+  if (index == pokeArray.length) {
+    numElement2.innerHTML = pokeArray[index].name;
+    picElement2.src = pokeArray[index].image;
+    index = 0;
+  }
 }
+// Box 2
+// function pokeCounting2() {
+//  let nameElement2 = document.getElementById('pokeNames2');
+//  nameElement2.innerHTML = pokeArray[index].name;
+//  let picElement2 = document.getElementById('pokePics2');
+// picElement2.src = pokeArray[index].image;
+//  let numElement2 = document.getElementById('num2');
+//  numElement2.innerHTML = index;
+//  index++;
+// }
